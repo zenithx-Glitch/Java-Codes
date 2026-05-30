72b87230822064b7981b3cef7766903a0c3549c5
zenith <satyam.zenithx@gmail.com>
2026-05-30 07:12:49 +0000

    Added basic calculation code

---
+++ basiccal/basiccal.java
@@ -0,0 +1,8 @@
+public class basiccal{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println("The sum of "+a+" and "+b+" is "+sum);
+    }
+}
