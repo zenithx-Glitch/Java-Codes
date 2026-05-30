82df18d8a7df737636909e96d226bd4f08a707d9
zenith <satyam.zenithx@gmail.com>
2026-05-30 13:26:24 +0000

    Conditional statement added

---
+++ conditionalstatment/condition.java
@@ -0,0 +1,14 @@
+public class condition{
+    public static void main(String[] args) {
+        int a = 10;
+        if(a > 0){
+            System.out.println("The number is positive");
+        }
+        else if(a < 0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
